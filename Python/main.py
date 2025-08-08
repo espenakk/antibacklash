9d39afc4e7a972eb7c35987b7f279afc31d0e4b0
Rune Espenakk <espenakk@outlook.com>
2025-08-08 14:24:08 +0200

    Added log11.csv

--- Python/main.py
+++ Python/main.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 
 # Load the data
-file_path = 'Python/data/newestlog.csv'
+file_path = 'Python/data/log11.csv'
 try:
     df = pd.read_csv(file_path)
 except FileNotFoundError:
